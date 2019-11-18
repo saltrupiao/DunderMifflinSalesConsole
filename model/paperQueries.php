2e5377c21ba19d337ec45393be003086da65450c
brendan_admin <brendan_admin@secs.oakland.edu>
2019-11-18 15:57:20 -0500

    Completed paper queries.

---
+++ model/paperQueries.php
@@ -0,0 +1,75 @@
+function insert($paper) {
+    global $db;
+    
+    $query = 'INSERT INTO paper
+                (PPR_TYPE,PPR_SIZE,PPR_COLOR,PPR_WEIGHT,PPR_IMG,PPR_QOH,
+                PPR_PRICE) 
+              VALUES 
+                (:pprType,:pprSize,:pprColor,:pprWeight,:pprImg,:pprState,
+                :pprPrice)';
+    try {
+        $statement = $db->prepare($query);
+        $statement->bindValue(':pprType', $paper->getPprType());
+        $statement->bindValue(':pprSize', $paper->getPprSize());
+        $statement->bindValue(':pprColor', $paper->getPprColor());
+        $statement->bindValue(':pprWeight', $paper->getPprWeight());
+        $statement->bindValue(':pprImg', $paper->getPprImg());
+        $statement->bindValue(':pprQOH', $paper->getPprQOH());
+        $statement->bindValue(':pprPrice', $paper->getPprPrice());
+        $statement->execute();
+        $statement->closeCursor();
+        
+        // Get the last product ID that was inserted
+        $ppr_id = $db->lastInsertId();
+        return $ppr_id;
+    } catch (PDOException $e) {
+        exit;
+    }
+}
+
+function update($paper) {
+    global $db;
+    $query = 'UPDATE paper
+              SET PPR_TYPE = :pprType,
+                  PPR_SIZE = :pprSize,
+                  PPR_COLOR = :pprColor,
+                  PPR_WEIGHT = :pprWeight,
+                  PPR_IMG = :pprImg,
+                  PPR_QOH = :pprQOH,
+                  PPR_PRICE = :pprPrice,
+              WHERE PPR_ID = :pprID';
+    try {
+        $statement = $db->prepare($query);
+        $statement->bindValue(':pprID', $paper->getPprID());
+        $statement->bindValue(':pprType', $paper->getPprType());
+        $statement->bindValue(':pprTYPE', $paper->getPprType());
+        $statement->bindValue(':pprColor', $paper->getPprColor());
+        $statement->bindValue(':pprWeight', $paper->getPprWeight());
+        $statement->bindValue(':pprImg', $paper->getPprImg());
+        $statement->bindValue(':pprState', $paper->getPprState());
+        $statement->bindValue(':pprPrice', $paper->getPprPrice());
+        $statement->bindValue(':pprStreet', $paper->getPprStreet());
+        $statement->bindValue(':pprZipcode', $paper->getPprZipcode());
+        $statement->bindValue(':pprLstMod', $paper->getPprLstMod());
+        $row_count = $statement->execute();
+        $statement->closeCursor();
+        
+        return $row_count;
+    } catch (PDOException $e) {
+        exit;
+    }
+}
+
+function delete($pprID) {
+    global $db;
+    $query = 'DELETE FROM paper WHERE PPR_ID = :pprID';
+    try {
+        $statement = $db->prepare($query);
+        $statement->bindValue(':pprID', $pprID);
+        $row_count = $statement->execute();
+        $statement->closeCursor();
+        return $row_count;
+    } catch (PDOException $e) {
+        exit;
+    }
+}
